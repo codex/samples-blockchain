ff9ab5f1df70e188299dd227f811317059b725bc
Nishant Sonar <nishant_sonar@yahoo.com>
2018-03-14 16:53:45 +0530

    Transactions are submitted , but with payload serialization error.

--- sawtooth/sample-sawtooth-java/src/main/java/BatchSender.java
+++ sawtooth/sample-sawtooth-java/src/main/java/BatchSender.java
@@ -2,40 +2,64 @@ import com.google.protobuf.ByteString;
 import com.mashape.unirest.http.Unirest;
 import sawtooth.sdk.processor.Utils;
 import sawtooth.sdk.protobuf.*;
-
+import sawtooth.sdk.client.Signing;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
 import java.security.Signature;
+import java.security.interfaces.DSAPrivateKey;
 import java.security.spec.ECGenParameterSpec;
+
+
 import org.apache.commons.codec.binary.Hex;
+import org.apache.http.ssl.PrivateKeyDetails;
+import org.bitcoinj.core.ECKey;
+import org.spongycastle.crypto.params.DSAPrivateKeyParameters;
+import org.spongycastle.crypto.util.PrivateKeyFactory;
 public class BatchSender {
 
     public static void main(String[] args) throws Exception{
 
 
-        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("EC");
-        ECGenParameterSpec parameterSpec = new ECGenParameterSpec("secp256k1");
+        //KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("EC");
+        //ECGenParameterSpec parameterSpec = new ECGenParameterSpec("secp256k1");
+        
+        //keyPairGenerator.initialize(parameterSpec);
 
-        keyPairGenerator.initialize(parameterSpec);
+        //KeyPair keyPair = keyPairGenerator.generateKeyPair(); // generate the originator or signer key pair.
+       
+        //Signature ecdsaSign = Signature.getInstance("SHA256withECDSA");
 
-        KeyPair keyPair = keyPairGenerator.generateKeyPair(); // generate the originator or signer key pair.
-        System.out.println("Key Pair Generated PVT as :"+keyPair.getPrivate());
-        System.out.println("Key Pair Generated PUB as :"+keyPair.getPublic());
-        Signature ecdsaSign = Signature.getInstance("SHA256withECDSA");
+        //ecdsaSign.initSign(keyPair.getPrivate());
+        
+        //byte[] publicKeyBytes = keyPair.getPublic().getEncoded();
+        
+        
+//        String privateKeyWif = "51114cac71a9575bc1b39104d176a39d81bd1a705b9a1ad32efd2222f13e59ad";
+//        ECKey privateKey = Signing.readWif(privateKeyWif);
+//
+//        String expectedPublicKey = "025fe2d166a5a8ff005eb0c799a474174f5d061de266438c69d36c2032c6bff51a";
+//        String calculatedPublicKey = Signing.getPublicKey(privateKey);
+//        assert expectedPublicKey.equals(calculatedPublicKey);
 
-        ecdsaSign.initSign(keyPair.getPrivate());
+    	ECKey privateKey = Signing.generatePrivateKey(null);  // new random privatekey
+    	String publicKeyHex = privateKey.getPublicKeyAsHex();
+    			
+    	System.out.println("Key Pair Generated PVT as :"+privateKey.getPrivateKeyAsHex());
+        System.out.println("Key Pair Generated PUB as :"+publicKeyHex);
+
+        //byte[] publicKeyBytes =    calculatedPublicKey.getBytes();
+        //byte[] publicKeyBytes =    privateKey.getPubKey();
+        
+        //String publicKeyHex = Utils.hash512(publicKeyBytes);
+        ByteString publicKeyByteString = ByteString.copyFrom(new String(publicKeyHex),"UTF-8");
 
 
-        byte[] publicKeyBytes = keyPair.getPublic().getEncoded();
-        String publicKeyHex = Hex.encodeHexString(publicKeyBytes);
-
-        ByteString publicKeyByteString = ByteString.copyFrom(new String(publicKeyBytes),"UTF-8");
-
-
-        String payload = "{'Name':'foo', 'Value':'bar'}";  // the actual payload data.
+        String payload = "{'Name':'sonar', 'Value':'some_value'}";  // the actual payload data.
         String payloadBytes = Utils.hash512(payload.getBytes());
 
         ByteString payloadByteString  = ByteString.copyFrom(payload.getBytes());
+        //ByteString payloadByteString  = ByteString.copyFrom(payloadBytes.getBytes());
         
         
 
@@ -48,21 +72,25 @@ public class BatchSender {
                 setNonce("1").
                 addOutputs("1cf1266e282c41be5e4254d8820772c5518a2c5a8c0c7f7eda19594a7eb539453e1ed7").
                 //setPayloadEncoding("application/json").
+                //setPayloadSha512Bytes(payloadByteString).
                 setPayloadSha512(payloadBytes).
                 setSignerPublicKey(publicKeyHex).
                 build();
 
 
         ByteString txnHeaderBytes = txnHeader.toByteString();
-        ecdsaSign.update(txnHeaderBytes.toByteArray());
-        byte[] txnHeaderSignature = ecdsaSign.sign();
+        //ecdsaSign.update(txnHeaderBytes.toByteArray());
+        byte[] txnHeaderSignature = privateKey.signMessage(txnHeaderBytes.toString()).getBytes();
 
 
 
+		String value =Signing.sign(privateKey, txnHeader.toByteArray()); 
 		Transaction txn = Transaction.newBuilder()
 				.setHeader(txnHeaderBytes)
 				.setPayload(payloadByteString)
-				.setHeaderSignature(Hex.encodeHexString(txnHeaderSignature)).build();
+				//.setHeaderSignature(Utils.hash512(txnHeaderSignature))
+				.setHeaderSignature(value)
+				.build();
 
         BatchHeader batchHeader = BatchHeader
         		.newBuilder()
@@ -75,13 +103,15 @@ public class BatchSender {
 
 
 
-        ecdsaSign.update(batchHeaderBytes.toByteArray());
+        //ecdsaSign.update(batchHeaderBytes.toByteArray());
 
-        byte[] batchHeaderSignature = ecdsaSign.sign();
-
+        //byte[] batchHeaderSignature = ecdsaSign.sign();
+        byte[] batchHeaderSignature = privateKey.signMessage(batchHeaderBytes.toString()).getBytes();
+        String value_batch =Signing.sign(privateKey, batchHeader.toByteArray());
         Batch batch = Batch.newBuilder()
         		.setHeader(batchHeaderBytes)
-        		.setHeaderSignature(Hex.encodeHexString(batchHeaderSignature))
+        		//.setHeaderSignature(Utils.hash512(batchHeaderSignature))
+        		.setHeaderSignature(value_batch)
         		.setTrace(true)
         		.addTransactions(txn)
         		.build();
@@ -98,5 +128,9 @@ public class BatchSender {
         System.out.println(serverResponse);
     }
 
+    
+    public void getSigner() {
+    	
+    }
 
 }
